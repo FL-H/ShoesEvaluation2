28dfbb90c912e347f16c6ce3b51ba54b7e374da7
FL-H <140992728+FL-H@users.noreply.github.com>
2023-10-29 16:26:03 +0900

    Add files via upload

--- ShoesEvaluation/index.js
+++ ShoesEvaluation/index.js
@@ -1,4 +1,32 @@
 'use strict';
-console.log('hello, world!');
 
-window.location.href = 'indexV2.html';
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+    var target = '';
+    switch (req.url) {
+        case '/':
+        case '/index':
+            target = './indexV2.html';
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('bad request');
+            return;
+    }
+
+
+    fs.readFile(target, 'utf-8', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+    })
+});
+
+server.listen(3000);
+console.log('サーバーを起動しました。');
+
+
+// console.log('hello, world!');
+
