0c40a47e913b9c704f8c6e5a690402092f0be912
FL-H <140992728+FL-H@users.noreply.github.com>
2023-10-29 16:53:32 +0900

    Add files via upload

--- ShoesEvaluation/index.js
+++ ShoesEvaluation/index.js
@@ -22,11 +22,11 @@ var server = http.createServer(function (req, res) {
         res.end();
     })
 
-    fs.readFile('./css/styleV2.css', 'UTF-8', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        res.end();
-    })
+    // fs.readFile('./css/styleV2.css', 'UTF-8', function (err, data) {
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.write(data);
+    //     res.end();
+    // })
 });
 
 server.listen(3000);
