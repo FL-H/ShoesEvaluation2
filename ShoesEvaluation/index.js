c597ac2b5e26d64d386dab7d0557cfaa12f69987
FL-H <140992728+FL-H@users.noreply.github.com>
2023-10-29 16:47:25 +0900

    Add files via upload

--- ShoesEvaluation/index.js
+++ ShoesEvaluation/index.js
@@ -22,7 +22,7 @@ var server = http.createServer(function (req, res) {
         res.end();
     })
 
-    fs.readFile('./css/test.css', 'UTF-8', function (err, data) {
+    fs.readFile('./css/styleV2.css', 'UTF-8', function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         res.end();
